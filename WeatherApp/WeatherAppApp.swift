3c3dcf987aba3c8c67c7c4b4a0c15a4bd4af4912
Aecio Lima <aeclean@gmail.com>
2024-12-18 05:07:42 -0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Aecio Lima on 18/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
